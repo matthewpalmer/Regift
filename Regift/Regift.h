80d01af8d8d896ce0e9f6fe472b61cd68df5a94f
matthewpalmer <matt@matthewpalmer.net>
2014-12-27 18:42:57 +1100

    Initial Commit

---
+++ Regift/Regift.h
@@ -0,0 +1,19 @@
+//
+//  Regift.h
+//  Regift
+//
+//  Created by Matthew Palmer on 27/12/2014.
+//  Copyright (c) 2014 Matthew Palmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Regift.
+FOUNDATION_EXPORT double RegiftVersionNumber;
+
+//! Project version string for Regift.
+FOUNDATION_EXPORT const unsigned char RegiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Regift/PublicHeader.h>
+
+
