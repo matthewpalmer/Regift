8b1274c8348311c6933d956f495dd5f7c8bee636
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2016-03-09 20:52:20 +0900

    In OS X, Compiler needs to read Regift.h

--- Regift/Regift.h
+++ Regift/Regift.h
@@ -6,7 +6,13 @@
 //  Copyright (c) 2014 Matthew Palmer. All rights reserved.
 //
 
+#if TARGET_OS_WATCH
+#elif TARGET_OS_TV
+#elif TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
+#elif TARGET_OS_MAC
+#import <Cocoa/Cocoa.h>
+#endif
 
 //! Project version number for Regift.
 FOUNDATION_EXPORT double RegiftVersionNumber;
