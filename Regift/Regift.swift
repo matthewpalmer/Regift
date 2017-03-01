70020fec37371389dc999f7f832d0c62da3ea377
Kira <1027610607@qq.com>
2017-03-01 18:12:22 +0800

    如果有某些帧没有取到也不抛出异常

--- Regift/Regift.swift
+++ Regift/Regift.swift
@@ -293,19 +293,11 @@ public struct Regift {
 
         // Create a dispatch group to force synchronous behavior on an asynchronous method.
         let gifGroup = Group()
-        var dispatchError: Bool = false
         gifGroup.enter()
 
         generator.generateCGImagesAsynchronously(forTimes: times, completionHandler: { (requestedTime, image, actualTime, result, error) in
-            // if not return when error occured and the time is the last time, gifGroup.leave() will get crash
-            guard !dispatchError else {
-                return
-            }
-            
             guard let imageRef = image , error == nil else {
                 print("An error occurred: \(error), image is \(image)")
-                dispatchError = true
-                gifGroup.leave()
                 return
             }
 
@@ -318,11 +310,6 @@ public struct Regift {
 
         // Wait for the asynchronous generator to finish.
         gifGroup.wait()
-
-        // If there was an error in the generator, throw the error.
-        if dispatchError {
-            throw RegiftError.AddFrameToDestination
-        }
         
         CGImageDestinationSetProperties(destination, fileProperties as CFDictionary)
         
